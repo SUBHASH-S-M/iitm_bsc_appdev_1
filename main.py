7016c38911608e19ff407600a9ff28418d5cc5b4
SUBHASH-S-M <69983414+SUBHASH-S-M@users.noreply.github.com>
2023-09-23 15:29:49 +0000

    code addition

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 print("hello world")
+with open('yourfile.mp3', 'rb') as mp3_file:
+  mp3_data = mp3_file.read()
+
+# Save the binary MP3 data to a file
+with open('saved_file.mp3', 'wb') as saved_file:
+  saved_file.write(mp3_data)
